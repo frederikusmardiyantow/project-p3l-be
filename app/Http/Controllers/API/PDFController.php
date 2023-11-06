e9d237660ec9e64084bf2a27086ff07af19a1369
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2023-11-07 00:32:42 +0700

    add PDFController + tampilan pdf (blade)

---
+++ app/Http/Controllers/API/PDFController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\MasterTrxReservasi;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use PDF;
+
+class PDFController extends Controller
+{
+    public function exportPDF(string $id) {
+
+        $cekReservasi = MasterTrxReservasi::with(["trxKamars", "trxKamars.jenisKamars", "trxKamars.kamars" ,"customers", "pic", "fo"])->find($id);
+        if(!$cekReservasi){
+            return response([
+                'status' => 'F',
+                'message' => 'Data Trx Reservasi tidak ditemukan!'
+            ], 404);
+        }
+        if($cekReservasi['status'] == 'Menunggu Pembayaran'){
+            return response([
+                'status' => 'F',
+                'message' => 'Maaf, Reservasi belum dibayar!'
+            ], 403);
+        }
+        if($cekReservasi['status'] == 'Batal'){
+            return response([
+                'status' => 'F',
+                'message' => 'Reservasi sudah dibatalkan!'
+            ], 403);
+        }
+        // hitung jumlah masing-masing jenis kamar yang dipesan dan total harga
+        $jumlahKamarPerJenis = [];
+        $totalHarga = 0;
+        foreach ($cekReservasi->trxKamars as $trx) {
+            $jenisKamar = $trx->jenisKamars->jenis_kamar;
+            $hargaPerMalam = $trx->harga_per_malam;
+            // $totalPerJenis = $hargaPerMalam;
+            $totalHarga += $trx->harga_per_malam;
+        
+            if (array_key_exists($jenisKamar, $jumlahKamarPerJenis)) {
+                $jumlahKamarPerJenis[$jenisKamar]->jumlah += 1;
+                $jumlahKamarPerJenis[$jenisKamar]->total_per_jenis += $hargaPerMalam;
+            } else {
+                $jumlahKamarPerJenis[$jenisKamar] = (object) [
+                    'jenis_kamar' => $jenisKamar,
+                    'jumlah' => 1,
+                    'harga_per_malam' => $hargaPerMalam,
+                    'total_per_jenis' => $hargaPerMalam,
+                ];
+            }
+        }        
+        // return response([
+        //     'data' => $jumlahKamarPerJenis
+        // ]);
+        
+        // gunakan carbon untuk ngeformat tanggal
+        $tanggal_cetak = Carbon::now();
+        $waktu_reservasi = Carbon::parse($cekReservasi['waktu_reservasi']);
+        $check_in = Carbon::parse($cekReservasi['waktu_check_in']);
+        $check_out = Carbon::parse($cekReservasi['waktu_check_out']);
+        $tanggal_pembayaran = Carbon::parse($cekReservasi['waktu_pembayaran']);
+
+        $data = [
+            'id_booking' => $cekReservasi['id_booking'],
+            'tanggal_reservasi' => $waktu_reservasi->format('d F Y H:i:s'),
+            'nama' => $cekReservasi->customers->nama_customer,
+            'alamat' => $cekReservasi->customers->alamat,
+            'check_in' => $check_in->format('d F Y H:i:s'),
+            'check_out' => $check_out->format('d F Y H:i:s'),
+            'dewasa' => $cekReservasi->jumlah_dewasa,
+            'anak_anak' => $cekReservasi->jumlah_anak_anak,
+            'tanggal_cetak' => $tanggal_cetak->format('d M Y H:i:s'),
+            'tanggal_pembayaran' => $tanggal_pembayaran->format('d F Y H:i:s'),
+            'total_harga' => $totalHarga,
+            'jumlahKamarPerJenis' => $jumlahKamarPerJenis,
+            'req_layanan' => $cekReservasi->req_layanan
+        ]; // Data yang akan dimasukkan ke dalam PDF
+
+        $pdf = PDF::loadView('tanda-terima', $data);
+
+        // Tambahkan gambar ke dalam PDF
+        $pdf->getDomPDF()->getOptions()->set('isPhpEnabled', true);
+        $pdf->getDomPDF()->getOptions()->set('isHtml5ParserEnabled', true);
+
+        $pdf->getDomPDF()->loadHtml(view('tanda-terima', $data)->render());
+
+        // Render PDF
+        $pdf->getDomPDF()->render();
+
+        return $pdf->stream('tanda-terima-pemesanan.pdf');
+
+        // $data = [];
+
+        // $pdf = PDF::loadView('tanda-terima', $data);
+        // return $pdf->download('tanda-terima-pemesanan.pdf');
+    }
+
+}
---
+++ resources/views/tanda-terima.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tanda Terima Pemesanan</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="header">
+        <img src="GAH-horizontal.png" alt="logo" class="gambar-logo">
+        <p class="alamat">Jl. Babarsari No.43, Janti, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
+    </div>
+    <div class="body mt-4">
+        <p class="judul mb-4">Tanda Terima Pemesanan</p>
+        <div class="mb-4">
+            <table class="table table-borderless">
+                <tbody>
+                    <tr>
+                        <td style="text-align: left">ID Booking : {{ $id_booking }}</td>
+                        <td style="text-align: right">Tanggal    : {{ $tanggal_reservasi }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-4">
+            <table class="table table-borderless">
+                <tbody>
+                    <tr>
+                        <td style="width: 30%">Nama</td>
+                        <td style="width: 2px">:</td>
+                        <td>{{ $nama }}</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>{{ $alamat }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-4">
+            <p class="font-weight-bold" style="font-size: 18px">Detail Pemesanan</p>
+            <table class="table table-borderless">
+                <tbody>
+                    <tr>
+                        <td style="width: 30%">Check In</td>
+                        <td style="width: 2px">:</td>
+                        <td>{{ $check_in }}</td>
+                    </tr>
+                    <tr>
+                        <td>Check Out</td>
+                        <td>:</td>
+                        <td>{{ $check_out }}</td>
+                    </tr>
+                    <tr>
+                        <td>Dewasa</td>
+                        <td>:</td>
+                        <td>{{ $dewasa }}</td>
+                    </tr>
+                    <tr>
+                        <td>Anak-anak</td>
+                        <td>:</td>
+                        <td>{{ $anak_anak }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Pembayaran</td>
+                        <td>:</td>
+                        <td>{{ $tanggal_pembayaran }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-4">
+            <p class="font-weight-bold" style="font-size: 18px">Rincian</p>
+            <table class='table table-bordered lh-base'>
+                <thead>
+                    <tr style="text-align: center">
+                        <th>No.</th>
+                        <th>Jenis Kamar</th>
+                        <th>Jumlah</th>
+                        <th>Harga</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php $i=1 @endphp
+                    @foreach($jumlahKamarPerJenis as $data)
+                    <tr style="text-align: center">
+                        <td>{{ $i++ }}</td>
+                        <td>{{ $data->jenis_kamar }}</td>
+                        <td>{{ $data->jumlah }}</td>
+                        <td style="text-align: right;">{{ "Rp " . number_format($data->harga_per_malam, 0, ',', '.') . ",-" }}</td>
+                        <td style="text-align: right">{{ "Rp " . number_format($data->total_per_jenis, 0, ',', '.') . ",-" }}</td>
+                    </tr>
+                    @endforeach
+                    <tr style="text-align: right">
+                        <td colspan="4"></td>
+                        <td style="font-weight: bold">{{ "Rp " . number_format($total_harga, 0, ',', '.') . ",-" }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-4">
+            <p class="font-weight-bold" style="font-size: 18px">Permintaan Khusus</p>
+            @if ($req_layanan == null)
+                <p class="ml-4 text-muted font-italic">- Tidak ada data permintaan khusus -</p>
+            @else
+                <ul>
+                    @foreach($req_layanan as $item)
+                        <li>-. {{ $item }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+
+    </div>
+    
+    <p class="mt-5" style="font-style: italic; font-size: 13px">dicetak pada tanggal {{ $tanggal_cetak }}</p>
+</body>
+</html>
