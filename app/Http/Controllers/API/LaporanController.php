c61fbaff0664e6de22109e0b41f0c88289a4ccd0
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2023-11-28 16:42:41 +0700

    upt laporan pendapatan per bulan fix

--- app/Http/Controllers/API/LaporanController.php
+++ app/Http/Controllers/API/LaporanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use App\Models\MasterCustomer;
 use App\Models\MasterTrxReservasi;
 use Carbon\Carbon;
@@ -90,23 +91,125 @@ class LaporanController extends Controller
 
     public function laporanPendapatanPerJenisTamuPerBulan(string $tahun)
     {
-        $data = MasterTrxReservasi::select(
+        $mergedResults = [];
+        $monthNames = [
+            1 => 'Januari',
+            2 => 'Februari',
+            3 => 'Maret',
+            4 => 'April',
+            5 => 'Mei',
+            6 => 'Juni',
+            7 => 'Juli',
+            8 => 'Agustus',
+            9 => 'September',
+            10 => 'Oktober',
+            11 => 'November',
+            12 => 'Desember',
+        ];
+
+        $dataReservasi = MasterTrxReservasi::select(
             DB::raw('MONTH(master_trx_reservasis.waktu_check_out) as bulan'),
-            'master_customers.jenis_customer',
-            DB::raw('SUM(master_trx_reservasis.total_harga) as total_pendapatan')
+            'master_customers.jenis_customer as type',
+            DB::raw('SUM(master_trx_reservasis.uang_jaminan) as dp')
         )
         ->join('master_customers', 'master_customers.id', '=', 'master_trx_reservasis.id_customer')
-        // ->join('trx_layanan_berbayars', function ($join) {
-        //     $join->on('trx_layanan_berbayars.id_trx_reservasi', '=', 'master_trx_reservasis.id');
-        // })
-        ->where('master_trx_reservasis.status', 'Out') // Menambahkan nama tabel pada status
-        ->whereYear('master_trx_reservasis.waktu_check_out', $tahun)
-        ->groupBy('bulan', 'master_customers.jenis_customer')
+        ->whereYear('master_trx_reservasis.waktu_pembayaran', $tahun)
+        ->groupBy('bulan', 'type')
         ->get();
+
+        $resultReservasi = [];
+        // Iterate through months and types
+        for ($bulan = 1; $bulan <= 12; $bulan++) {
+            foreach (['G', 'P'] as $type) {
+                // Check if there is a matching record in the existing result set
+                $matchingRecord = $dataReservasi->first(function ($record) use ($bulan, $type) {
+                    return $record->bulan == $bulan && $record->type == $type;
+                });
+
+                // Set DP value based on the presence of a matching record
+                $dp = $matchingRecord ? $matchingRecord->dp : 0;
+
+                // Add the result to the final array
+                $resultReservasi[] = [
+                    'bulan' => $bulan,
+                    'type' => $type,
+                    'dp' => $dp,
+                ];
+            }
+        }
+        foreach ($resultReservasi as $reservasi) {
+            $no = $reservasi['bulan'];
+            $month = $monthNames[$reservasi['bulan']];
+        
+            if (!isset($mergedResults[$no])) {
+                $mergedResults[$no] = [
+                    'no' => $no,
+                    'bulan' => $month,
+                    'P' => 0,
+                    'G' => 0,
+                    'total' => 0,
+                ];
+            }
+        
+            $mergedResults[$no][$reservasi['type']] += $reservasi['dp'];
+        }
+
+        $dataInvoice = Invoice::select(
+            DB::raw('MONTH(invoices.tgl_lunas) as bulan'),
+            DB::raw('LEFT(invoices.no_invoice,1) as type'),
+            DB::raw('SUM((invoices.total_harga_kamar - (master_trx_reservasis.uang_jaminan)) + invoices.total_harga_layanan) as total')
+        )
+        ->join('master_trx_reservasis', 'master_trx_reservasis.id', '=', 'invoices.id_trx_reservasi')
+        ->whereYear('invoices.tgl_lunas', $tahun)
+        ->groupBy('bulan', 'type')
+        ->get();
+
+        $resultInvoice = [];
+        // Iterate through months and types
+        for ($bulan = 1; $bulan <= 12; $bulan++) {
+            foreach (['G', 'P'] as $type) {
+                // Check if there is a matching record in the existing result set
+                $matchingRecordIn = $dataInvoice->first(function ($record) use ($bulan, $type) {
+                    return $record->bulan == $bulan && $record->type == $type;
+                });
+
+                // Set total value based on the presence of a matching record
+                $total = $matchingRecordIn ? $matchingRecordIn->total : 0;
+
+                // Add the result to the final array
+                $resultInvoice[] = [
+                    'bulan' => $bulan,
+                    'type' => $type,
+                    'total' => $total,
+                ];
+            }
+        }
+        foreach ($resultInvoice as $invoice) {
+            $month = $invoice['bulan'];
+        
+            if (!isset($mergedResults[$month])) {
+                $mergedResults[$month] = [
+                    'bulan' => $month,
+                    'P' => 0,
+                    'G' => 0,
+                    'total' => 0,
+                ];
+            }
+        
+            $mergedResults[$month][$invoice['type']] += $invoice['total'];
+        }
+
+        // Calculate the total for each month
+        foreach ($mergedResults as &$result) {
+            $result['total'] = $result['P'] + $result['G'];
+        }
+
+        // Convert the associative array to indexed array
+        $mergedResults = array_values($mergedResults);
 
         return response([
             'status' => 'T',
-            'data' => $data,
+            'data' => $mergedResults,
         ], 200);
     }
 }
